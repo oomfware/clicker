db0f2581879a0e2184a047b72a1d4f5030ca52fe
Mary <git@mary.my.id>
2026-04-06 19:42:49 +0700

    refactor: mark ID functions as pure

--- packages/protocol/src/ids.ts
+++ packages/protocol/src/ids.ts
@@ -2,7 +2,14 @@ import { nanoid } from 'nanoid';
 
 const createPrefixedId = (prefix: string, size: number): string => `${prefix}${nanoid(size)}`;
 
+/*#__NO_SIDE_EFFECTS__*/
 export const createWorkspaceId = (): string => createPrefixedId('ws_', 6);
+
+/*#__NO_SIDE_EFFECTS__*/
 export const createSessionId = (): string => createPrefixedId('sess_', 8);
+
+/*#__NO_SIDE_EFFECTS__*/
 export const createConnectionId = (): string => createPrefixedId('conn_', 6);
+
+/*#__NO_SIDE_EFFECTS__*/
 export const createMessageId = (): string => createPrefixedId('msg_', 8);
